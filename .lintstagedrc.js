62f553ef3b647e2915ed3a0bf1396c778a616ff1
DESKTOP-70DNV3O\user <bhlee@aivis.kr>
2023-11-14 19:43:39 +0900

    chore: storybook 적용

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,10 @@
-module.exports = {
-  // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
+const path = require('path');
 
-  // Lint & Prettify TS and JS files
-  '**/*.(ts|tsx)': (filenames) => {
-    return [
-      `npx eslint --fix ${filenames.join(' ')}`,
-      `npx prettier --write ${filenames.join(' ')}`,
-      `npx stylelint --ignore-path .gitignore ${filenames.join(' ')}`,
-    ];
-  },
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
 };
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const FooterContainer = styled.section`
   h2 {
     font-size: 2rem;
-    color: green;
+    color: yellow;
   }
 `;
 
